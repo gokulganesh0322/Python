f078a848ea5bb30b0c4459f058a3d3e6229d52bb
[gokul] <[gokulganesh.pondy@gmail.com]>
2026-02-09 12:03:53 +0530

    Demo

--- python.py
+++ python.py
@@ -52,3 +52,116 @@ print(d)
 
 
 print("Hello")
+
+
+# print ("hello")
+# print("Hello")
+# print("Hello")
+
+
+# name = "Python"
+# print(name)
+# print(name)
+
+# first_name = "lqjhdw" 
+
+# print(First_name)
+
+
+# print("Hello Python")
+# print('Hello')
+
+
+
+# name = input("Please Enter Your Name: ")
+# print(name)
+
+
+# name = "Python"
+# print(type(name))
+
+
+# num  = 10
+# print(type(num))
+
+# anothernum = 10.5
+# print(type(anothernum))
+
+# active = True
+# print(type(active))
+
+# print("Hello")
+
+# name = "Python"
+
+
+
+# name = "Python"
+# print(name," ","Is my Programming Language")
+
+
+a=10
+b=20
+c=30
+d=a+b+c
+print(d)
+
+
+print("Hello")
+
+
+
+# print ("hello")
+# print("Hello")
+# print("Hello")
+
+
+# name = "Python"
+# print(name)
+# print(name)
+
+# first_name = "lqjhdw" 
+
+# print(First_name)
+
+
+# print("Hello Python")
+# print('Hello')
+
+
+
+# name = input("Please Enter Your Name: ")
+# print(name)
+
+
+# name = "Python"
+# print(type(name))
+
+
+# num  = 10
+# print(type(num))
+
+# anothernum = 10.5
+# print(type(anothernum))
+
+# active = True
+# print(type(active))
+
+# print("Hello")
+
+# name = "Python"
+
+
+
+# name = "Python"
+# print(name," ","Is my Programming Language")
+
+
+a=10
+b=20
+c=30
+d=a+b+c
+print(d)
+
+
+print("Hello")
